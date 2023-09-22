4c695a6ec34481f076cee07e70edb55be56ea03f
yongj.zhuang <zhuangyongj@gmail.com>
2023-09-23 01:27:53 +0800

    Formatting

--- vfm/client.go
+++ vfm/client.go
@@ -17,12 +17,12 @@ var (
 )
 
 type FstoreFile struct {
-	Id         int64         `json:"id"`
-	FileId     string        `json:"fileId"`
-	Name       string        `json:"name"`
-	Status     string        `json:"status"`
-	Size       int64         `json:"size"`
-	Md5        string        `json:"md5"`
+	Id         int64       `json:"id"`
+	FileId     string      `json:"fileId"`
+	Name       string      `json:"name"`
+	Status     string      `json:"status"`
+	Size       int64       `json:"size"`
+	Md5        string      `json:"md5"`
 	UplTime    miso.ETime  `json:"uplTime"`
 	LogDelTime *miso.ETime `json:"logDelTime"`
 	PhyDelTime *miso.ETime `json:"phyDelTime"`
